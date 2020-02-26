662481a22c7968e07688b194f41d2a84de792f4d
Octopus-free <ivashkinrg@gmail.com>
2020-02-26 21:06:20 +0300

    Second

--- mld/dbcreate/views.py
+++ mld/dbcreate/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from .forms import AlgoName
+from .models import AlgorithmsBook
 
 
 def index(request):
-    return render(request, 'dbcreate/index.html', context={})
+    form = AlgoName()
+    return render(request, 'dbcreate/index.html', context={'form': form})
+
+
+def contacts(request):
+    return render(request, 'dbcreate/contacts.html', context={})
+
+
+def about(request):
+
+    return render(request, 'dbcreate/about.html', context={})
+
+
+def db_knowledge(request):
+
+    select_from_db_algo_book = AlgorithmsBook.objects.all()
+    return render(request, 'dbcreate/db_knowledge.html', context={'algo_book': select_from_db_algo_book})
